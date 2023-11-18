5210c61bc80224b1d154e03e9300ffeac7673bb4
krychyjaby <mihap1011@gmail.com>
2023-11-18 19:40:19 +0300

    хотьчтото

---
+++ check_string_brackets.py
@@ -0,0 +1,17 @@
+def check_string_brackets(text):
+    counter_1 = 0
+    counter_2 = 0
+    for word in text:
+        if word == '(':
+            counter_1
+        if word == ')':
+            counter_2 += 1
+
+    if counter_1 == counter_2:
+        print("Yes")
+    else:
+        print("NO")
+
+
+
+check_string_brackets(input("Введи скобочки: "))        
